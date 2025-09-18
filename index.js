c9199c4a6666e874c3b50ae1afe10ba13b79454a
Shankar S <shankar.s@biits.in>
2025-09-18 20:19:56 +0530

    cors enabled

--- index.js
+++ index.js
@@ -16,25 +16,42 @@ const chatRoutes = require('./routes/chatRoutes');
 const prisma = new PrismaClient()
 const app = express();
 const server = http.createServer(app);
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 
-// Configure Socket.IO
-// const io = socketIo(server, {
-//     cors: {
-//         origin: process.env.FRONTEND_URL || "http://localhost:3000",
-//         methods: ["GET", "POST"],
-//         credentials: true
-//     }
-// });
+const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    process.env.FRONTEND_DEPLOYED_URL
+];
+
 const io = socketIo(server, {
     cors: {
-        origin: "*"
+        origin: function (origin, callback) {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("CORS policy: Origin not allowed"));
+            }
+        },
+        methods: ["GET", "POST"],
+        credentials: true
     }
 });
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true); // allow Postman or server requests
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("CORS policy: Origin not allowed"));
+        }
+    },
+    credentials: true
+}));
 
 
 app.get("/test-connection", async (req, res) => {
